e286f31b565c3c0a7eda7d0f851bb1eb28ae01ec
Diego Mongay <diego.mongay@birkle-it.com>
2019-08-14 15:34:00 +0200

    first example nodejs

---
+++ workspace/hong-kong-university/first-example/index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x, y) => (2 * (x + y)),
+    area: (x, y) => (x * y)
+};
+
+var solveRect = function (l, b) {
+    console.log("Solvin for rectangle with l = " + l + "and b = " + b);
+
+    if (l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should greater than zero: l = " + l + ", and b = " + b);
+    } else {
+        console.log("The area if the rectangle is " + rect.area(l, b));
+        console.log("The perimeter of the rectangle is " + rect.perimeter(l, b));
+    }
+};
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
