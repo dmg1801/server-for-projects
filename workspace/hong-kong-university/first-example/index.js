81349e88f018d20662e8572f14a9d3d587f7e307
Diego Mongay <diego.mongay@birkle-it.com>
2019-08-14 15:42:48 +0200

    simpple node module

--- workspace/hong-kong-university/first-example/index.js
+++ workspace/hong-kong-university/first-example/index.js
@@ -1,7 +1,4 @@
-var rect = {
-    perimeter: (x, y) => (2 * (x + y)),
-    area: (x, y) => (x * y)
-};
+var rect = require('./rectangle');
 
 var solveRect = function (l, b) {
     console.log("Solvin for rectangle with l = " + l + "and b = " + b);
